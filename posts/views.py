1ddee88e97c2d7d6d29b817b000c6a757bbd1d2f
rafasilva9537 <rafasilva9537@gmail.com>
2024-12-21 12:51:09 -0300

    Deleting posts app, changed to blog app

--- posts/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def post(request):
-    return HttpResponse("Hey, this is the post response!!!")
