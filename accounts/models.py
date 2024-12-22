eb9e397b621297d75dac1a14d43287acfc5997a0
rafasilva9537 <rafasilva9537@gmail.com>
2024-12-22 15:17:01 -0300

    User follows and db design

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,13 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     biography = models.CharField(max_length=200)
+    folllows = models.ManyToManyField("self", through="Follow")
+
+class Follow(models.Model):
+    followed_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followed_users")
+    follower_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower_users")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint("followed_user", "follower_user", name="unique_user_follows")
+        ]
