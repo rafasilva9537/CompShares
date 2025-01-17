f5aedb1fc3d02c6a5e244ea24d705df1a88beea5
Rafael <rafasilva9537@gmail.com>
2025-01-17 17:29:57 -0300

    Add more homepage styling

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from utils.fake_blogs import create_fake_blog
 # Create your views here.
 def home(request):
     # posts = Post.objects.order_by("-id")[:6]
-    posts = [ create_fake_blog() for _ in range(8)]
+    posts = [ create_fake_blog() for _ in range(7)]
 
     return render(request, "blog/home.html", {"posts": posts})
 
