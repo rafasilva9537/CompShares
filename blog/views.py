98f384385724031ffd6b1f4defaba885c872162a
Rafael <rafasilva9537@gmail.com>
2025-01-24 17:08:04 -0300

    Reorganize blog templates folder structure

--- blog/views.py
+++ blog/views.py
@@ -9,12 +9,12 @@ def home(request):
     # posts = Post.objects.order_by("-id")[:6]
     posts = [ create_fake_blog() for _ in range(7)]
 
-    return render(request, "blog/home.html", {"posts": posts})
+    return render(request, "blog/pages/home.html", {"posts": posts})
 
 def post(request, post_id):
     # TODO: find alternatives to not expose the post id
     post = get_object_or_404(Post, pk=post_id)
-    return render(request, "blog/post.html", {"post": post})
+    return render(request, "blog/pages/post.html", {"post": post})
 
 def login(request):
-    return render(request, "blog/login.html")
+    return render(request, "blog/pages/login.html")
