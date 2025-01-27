f2798a11d7598a6302624e9d13c31bf63962b3a5
Rafael <rafasilva9537@gmail.com>
2025-01-27 09:30:34 -0300

    Reorganize template and static directory structure
    Common templates in project folder and specific templates in app folders

--- website/settings.py
+++ website/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / "template" ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+STATIC_ROOT = BASE_DIR / "mydomain.com/static" #TODO: change in production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
